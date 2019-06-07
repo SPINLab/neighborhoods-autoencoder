144279772e82a8d09ac88faaaed56932bddad38d
reinvantveer <rein.van.t.veer@geodan.nl>
2019-06-07 15:37:16 +0200

    WIP: replace false max with true max

--- model/pytorch_efd.py
+++ model/pytorch_efd.py
@@ -13,14 +13,35 @@ def efd(polygon: torch.Tensor, order=10) -> torch.Tensor:
     assert isinstance(polygon, torch.Tensor), 'The polygon should be a tensor'
     assert polygon.size(1) == 2, 'The polygon should be of shape (?, 2): two coordinate axes'
 
+    # Add zero-padding in case the polygon doesn't have this
+    zeros = torch.zeros((4, 2), dtype=polygon.dtype)
+    polygon = torch.cat((polygon, zeros))
+
+    epsilon = torch.tensor(1e-16)
     # Following https://discuss.pytorch.org/t/equivalent-function-like-numpy-diff-in-pytorch/35327/2
     point_distances = polygon[1:] - polygon[:-1]  # pyefd: dxy
 
     lengths = torch.pow(point_distances, 2)  # pyefd: dt
     lengths = torch.sum(lengths, dim=(1,))  # pyefd: dt
-    lengths = torch.sqrt(lengths)  # pyefd: dt
+    lengths = torch.sqrt(lengths + epsilon)  # pyefd: dt
+
+    # Get rid of the epsilons added to zeros
+    lengths = lengths - torch.sqrt(epsilon)
 
     cumulative_lengths = torch.cumsum(lengths, dim=0)  # pyefd: t
+
+    # Replace the false maximum introduced by the zero padding with a true max
+    false_max = torch.max(cumulative_lengths)
+    rescale_factor = cumulative_lengths / (cumulative_lengths - false_max + epsilon)
+    reduced_cumsums = cumulative_lengths - false_max
+    rescaled_cumsums = reduced_cumsums * rescale_factor
+
+    # Replace the zeros with true max
+    true_max = torch.max(rescaled_cumsums)
+    rescale_factor = rescaled_cumsums / (rescaled_cumsums - true_max + epsilon)
+    rescaled_cumsums = rescaled_cumsums * rescale_factor  # TODO: this is / (rescale_factor + epsilon) or something
+    rescaled_cumsums = rescaled_cumsums + true_max
+
     zeros = torch.zeros((1,), dtype=torch.double)
     cumulative_lengths = torch.cat((zeros, cumulative_lengths))  # pyefd: t
     total_distance = cumulative_lengths[-1]  # pyefd: T
--- model/test_pytorch_efd.py
+++ model/test_pytorch_efd.py
@@ -13,14 +13,15 @@ class TestEllipticFourierDescriptor(unittest.TestCase):
     def test_efd(self):
         geom1 = 'POLYGON((0 0, 1 0, 1 1, 0 1, 0 0))'
         geom1 = gv.vectorize_wkt(geom1)
+        geom_extra_node = 'POLYGON((0 0, 1 0, 1 1, 0 1, 0 0.5, 0 0))'
 
         geom2 = 'POLYGON((1 1, 0 1, 0 0, 1 0, 1 1))'
-        geom_extra_node = 'POLYGON((0 0, 1 0, 1 1, 0 1, 0 0.5, 0 0))'
+        geom2 = gv.vectorize_wkt(geom2)
         polygon_tensor = geom1[:, :2]
         polygon_tensor = torch.from_numpy(polygon_tensor)
 
-        reference_descriptors = pyefd.elliptic_fourier_descriptors(geom1[:, :2], order=10)
-        stand_in_descriptors = elliptic_fourier_descriptors(geom1[:, :2], order=10)
+        reference_descriptors = pyefd.elliptic_fourier_descriptors(geom2[:, :2], order=10)
+        stand_in_descriptors = elliptic_fourier_descriptors(geom2[:, :2], order=10)
 
         with self.subTest('Our stand-in efd function does the same as pyefd'):
             np.testing.assert_array_equal(reference_descriptors, stand_in_descriptors)
@@ -34,9 +35,30 @@ class TestEllipticFourierDescriptor(unittest.TestCase):
                 efd(torch.rand((10, 3)))
 
         with self.subTest('It creates an elliptic fourier descriptor of a geometry, the same as pyefd creates'):
-            polygon_tensor = geom1[:, :2]
-            polygon_tensor = torch.from_numpy(polygon_tensor)
+            # polygon1_tensor = geom1[:, :2]
+            # polygon1_tensor = torch.from_numpy(polygon1_tensor)
+            # polygon1_efd = efd(polygon1_tensor, order=10).numpy()
 
-            our_descriptors = efd(polygon_tensor, order=10).numpy()
+            polygon2_tensor = geom2[:, :2]
+            polygon2_tensor = torch.from_numpy(polygon2_tensor)
+            polygon2_efd = efd(polygon2_tensor, order=10).numpy()
 
-            np.testing.assert_array_almost_equal(reference_descriptors, our_descriptors)
+            np.testing.assert_array_almost_equal(reference_descriptors, polygon2_efd)
+
+        with self.subTest('It handles inputs of zeros without nans'):
+            zero_coordinates = torch.zeros((10, 2), dtype=torch.double)
+            coeffs = efd(zero_coordinates)
+            coeffs = coeffs.numpy()
+
+            for element in coeffs.flatten():
+                self.assertFalse(np.isnan(element))
+
+        with self.subTest('It creates equal coefficients for zero-padded coordinate sequences'):
+            torch.manual_seed(42)
+            random_coordinates = torch.rand((4, 2), dtype=torch.double)
+            zero_padded_random_coords = torch.cat((random_coordinates, torch.zeros((4, 2), dtype=torch.double)))
+
+            non_zero_coeffs = efd(random_coordinates).numpy()
+            padded_coeffs = efd(zero_padded_random_coords).numpy()
+
+            np.testing.assert_array_almost_equal(non_zero_coeffs, padded_coeffs)
