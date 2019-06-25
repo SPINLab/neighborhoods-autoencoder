516a4b3ade88860efde079cf5edd4a1684d07993
Rein van 't Veer <rein.van.t.veer@geodan.nl>
2019-06-25 14:13:32 +0200

    log efd coefficients (for now)

--- js/src/polygonMap.js
+++ js/src/polygonMap.js
@@ -2,6 +2,7 @@ import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-draw';
 import 'leaflet-draw/dist/leaflet.draw.css';
+import efd from './efdCoefficients'
 
 /* This code is needed to properly load the images in the Leaflet CSS */
 delete L.Icon.Default.prototype._getIconUrl;
@@ -34,7 +35,15 @@ map.addControl(new L.Control.Draw({
 map.on(L.Draw.Event.CREATED, function (event) {
   const layer = event.layer;
   drawnItems.addLayer(layer);
-  const drawnCoords = event.layer.editing.latlngs[0][0];
+  const drawnPoints = event.layer.editing.latlngs[0][0];
+  console.log(drawnPoints);
+
+  // Reverse axis order to match x-longitude/y-latitude!
+  const coords = drawnPoints.map(latLng => [latLng.lng, latLng.lat]);
+  return efd(coords)
+    .then(console.log)
+
+
 });
 
 const defaultCenter = [0, 0];
